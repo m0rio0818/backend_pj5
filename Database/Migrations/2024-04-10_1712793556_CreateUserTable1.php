caceabca7fea69bebd4ab70ad02735f1ebaf5e04
m0rio0818 <72071848+m0rio0818@users.noreply.github.com>
2024-04-11 09:57:08 +0900

    ファイル名修正, migration追加

---
+++ Database/Migrations/2024-04-10_1712793556_CreateUserTable1.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Migrations;
+
+use Database\SchemaMigration;
+
+class CreateUserTable1 implements SchemaMigration
+{
+    // マイグレーションファイルを生成後、メソッドの処理を記述する
+    public function up(): array
+    {
+        // マイグレーション
+        return [
+            "CREATE TABLE users (
+                id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                username VARCHAR(255) NOT NULL,
+                email VARCHAR(255) NOT NULL UNIQUE,
+                password VARCHAR(255) NOT NULL,
+                created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+            )"
+        ];
+    }
+
+    public function down(): array
+    {
+        // ロールバック処理を書く
+        return [
+            "DROP TABLE users"
+        ];
+    }
+}
