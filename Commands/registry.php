83e5048483183271c5a49837cf0468372d596868
m0rio0818 <72071848+m0rio0818@users.noreply.github.com>
2024-04-15 11:39:51 +0900

    要素の追加

--- Commands/registry.php
+++ Commands/registry.php
@@ -2,8 +2,8 @@
 return [
     Commands\Programs\Migrate::class,
     Commands\Programs\CodeGeneration::class,
+    Commands\Programs\StateMigrate::class,
     Commands\Programs\DBWipe::class,
     Commands\Programs\BookSearch::class,
     Commands\Programs\GenerateCommands::class,
-    Commands\Programs\Ascent::class, 
 ];
